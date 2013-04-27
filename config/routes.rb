c912fe49431c75ca5b940ebec840b4ab272fa046
Billy Kovalsky <billy@yotpo.com>
2013-04-27 12:38:46 +0300

    init recive mails controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Mailmeonce::Application.routes.draw do
+  post "recive_mails" => "mails#new"
+
+
   post "users/create" => "users#create"
   get "users/new" => "users#new"
+
+  post "getmails" => "get_mails#save"
 
 
   get "home/index"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130426104934) do
+ActiveRecord::Schema.define(:version => 20130427092812) do
+
+  create_table "mails", :force => true do |t|
+    t.string   "from"
+    t.string   "to"
+    t.string   "subject"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
